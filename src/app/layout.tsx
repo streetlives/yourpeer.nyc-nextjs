02c5853cc086a997eb9c09f1ccd840c2439221e2
Jacob Beard <jake@minnow.io>
2024-07-10 16:56:02 -0600

    Fix fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;900&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body>
         {children}
         <GTranslateWrapper />
