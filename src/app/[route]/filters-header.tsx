271ee1ef65b69b3f55798cc687affa2df4e4ebe2
Jacob Beard <jake@minnow.io>
2024-07-11 23:45:55 -0600

    Fix <h1> on FiltersHeader - only active category is inside an <h1>

--- src/app/[route]/filters-header.tsx
+++ src/app/[route]/filters-header.tsx
@@ -36,35 +36,38 @@ export default function FiltersHeader({
   ];
   return (
     <div className="sticky top-0 w-full inset-x-0 bg-white z-10">
-      <h1 className="flex gap-2 py-3 px-4  flex-nowrap lg:flex-wrap items-center overflow-x-auto border-b border-dotted border-neutral-200 scrollbar-hide">
+      <div className="flex gap-2 py-3 px-4  flex-nowrap lg:flex-wrap items-center overflow-x-auto border-b border-dotted border-neutral-200 scrollbar-hide">
         {CATEGORIES.filter(
           (thisCategory) =>
             currentCategory === thisCategory || currentCategory === null,
-        ).map((thisCategory) => (
-          <Link
-            key={thisCategory}
-            className={classNames(
-              commonClassNames,
-              currentCategory === thisCategory
-                ? { "bg-primary": true }
-                : { "bg-neutral-100": true },
-            )}
-            href={
-              currentCategory === thisCategory
-                ? LOCATION_ROUTE
-                : CATEGORY_TO_ROUTE_MAP[thisCategory]
-            }
-          >
-            <img
-              src={getIconPath(CATEGORY_ICON_SRC_MAP[thisCategory])}
-              className="w-4 h-4"
-              alt=""
-            />
-            <span className="leading-3 truncate">
-              {CATEGORY_DESCRIPTION_MAP[thisCategory]}
-            </span>
-          </Link>
-        ))}
+        ).map((thisCategory) => {
+          const link = (
+            <Link
+              key={thisCategory}
+              className={classNames(
+                commonClassNames,
+                currentCategory === thisCategory
+                  ? { "bg-primary": true }
+                  : { "bg-neutral-100": true },
+              )}
+              href={
+                currentCategory === thisCategory
+                  ? LOCATION_ROUTE
+                  : CATEGORY_TO_ROUTE_MAP[thisCategory]
+              }
+            >
+              <img
+                src={getIconPath(CATEGORY_ICON_SRC_MAP[thisCategory])}
+                className="w-4 h-4"
+                alt=""
+              />
+              <span className="leading-3 truncate">
+                {CATEGORY_DESCRIPTION_MAP[thisCategory]}
+              </span>
+            </Link>
+          );
+          return currentCategory === thisCategory ? <h1>{link}</h1> : link;
+        })}
 
         <Link
           className="inline-flex flex-shrink-0 overflow-hidden items-center space-x-2 text-dark bg-neutral-100 rounded-full text-xs py-1 px-3"
@@ -81,7 +84,7 @@ export default function FiltersHeader({
           <img src="/img/icons/filters.svg" className="w-4 h-4" alt="" />
           <span className="leading-3 truncate">All Filters</span>
         </Link>
-      </h1>
+      </div>
     </div>
   );
 }
