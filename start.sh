25b8b598a4389360872b3b781ae73d216a99d355
Jacob Beard <jake@minnow.io>
2024-07-23 23:57:29 -0600

    fix strange validation hours text for new validations streetlives/yourpeer.nyc#176

--- start.sh
+++ start.sh
@@ -1,1 +1,2 @@
-GO_GETTA_PROD_URL=https://w6pkliozjh.execute-api.us-east-1.amazonaws.com/prod npm run dev -- --experimental-https
+#GO_GETTA_PROD_URL=https://w6pkliozjh.execute-api.us-east-1.amazonaws.com/prod npm run dev -- --experimental-https
+GO_GETTA_PROD_URL=https://w6pkliozjh.execute-api.us-east-1.amazonaws.com/Stage npm run dev -- --experimental-https
