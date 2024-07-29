a5e46ac7634d5c150db15af70fd51010b5739424
Sk <shakil@meghops.com>
2024-07-29 19:46:22 +0600

    fix #182, #184

--- src/components/location-detail-component.tsx
+++ src/components/location-detail-component.tsx
@@ -28,6 +28,7 @@ import { ReportIssueForm } from "./report-issue";
 import { parseCookies } from "./cookies";
 import QuickExitLink from "./quick-exit-link";
 import LocationStubMarker from "./location-stub-marker";
+import { Transition } from "@headlessui/react";
 
 export function getIconPath(iconName: string): string {
   return `/img/icons/${iconName}.png`;
@@ -88,7 +89,7 @@ function serializeToQueryParams(searchParams: SearchParams): string {
     .map(([k, v]) =>
       typeof v === "string"
         ? `${encodeURIComponent(k)}=${encodeURIComponent(v)}`
-        : "",
+        : ""
     )
     .join("&");
 }
@@ -111,24 +112,39 @@ export default function LocationDetailComponent({
   }
 
   const [previousRoute, setPreviousRoute] = useState<string>();
+  const [stickyTitle, setStickyTitle] = useState<boolean>(false);
   useEffect(() => {
     const cookies = parseCookies();
     if (cookies[LAST_SET_PARAMS_COOKIE_NAME]) {
       const previousParams = JSON.parse(
-        cookies[LAST_SET_PARAMS_COOKIE_NAME],
+        cookies[LAST_SET_PARAMS_COOKIE_NAME]
       ) as unknown as {
         searchParams: SearchParams;
         params: SubRouteParams;
       };
       setPreviousRoute(
-        `/${previousParams.params.route}${previousParams.params.locationSlugOrPersonalCareSubCategory ? `/${previousParams.params.locationSlugOrPersonalCareSubCategory}` : ""}${Object.keys(previousParams).length ? `?${serializeToQueryParams(previousParams.searchParams)}` : ""}`,
+        `/${previousParams.params.route}${previousParams.params.locationSlugOrPersonalCareSubCategory ? `/${previousParams.params.locationSlugOrPersonalCareSubCategory}` : ""}${Object.keys(previousParams).length ? `?${serializeToQueryParams(previousParams.searchParams)}` : ""}`
       );
     }
   }, []);
 
+  useEffect(() => {}, []);
+
+  const handleScroll = (e: React.UIEvent<HTMLElement>): void => {
+    const target = e.target as HTMLElement;
+    if (target.scrollTop > 30) {
+      setStickyTitle(true);
+    } else {
+      setStickyTitle(false);
+    }
+  };
+
   return (
-    <div className="details-screen bg-white md:block z-50 sm:z-0 fixed md:absolute inset-0 w-full h-full overflow-y-auto scrollbar-hide">
-      <div className="h-14 px-4 gap-x-2 flex justify-between md:justify-start items-center bg-white sticky top-0 left-0 w-full right-0">
+    <div
+      className="details-screen bg-white md:block z-50 sm:z-0 fixed md:absolute inset-0 w-full h-full overflow-y-auto scrollbar-hide"
+      onScroll={handleScroll}
+    >
+      <div className="h-14 px-4 gap-x-2 flex justify-between md:justify-start items-center bg-white sticky z-20 top-0 left-0 w-full right-0">
         <a
           className="text-dark hover:text-black transition flex-shrink-0"
           id="details_back"
@@ -150,6 +166,15 @@ export default function LocationDetailComponent({
             />
           </svg>
         </a>
+        <Transition show={stickyTitle}>
+          <h1
+            className="text-dark text-lg sm:text-xl font-medium truncate details-scroll-header transition duration-300 ease-in data-[closed]:opacity-0"
+            translate="no"
+          >
+            {location.name}
+          </h1>
+        </Transition>
+
         <QuickExitLink />
       </div>
       {isShowingReportIssueForm ? (
@@ -232,7 +257,7 @@ export default function LocationDetailComponent({
                       {locationStubs
                         ? locationStubs
                             .filter(
-                              (locationStub) => locationStub.id !== location.id,
+                              (locationStub) => locationStub.id !== location.id
                             )
                             .map((locationStub) => (
                               <LocationStubMarker
@@ -423,7 +448,7 @@ export default function LocationDetailComponent({
               {CATEGORIES.map((serviceCategory) => {
                 const servicesWrapper = getServicesWrapper(
                   serviceCategory,
-                  location,
+                  location
                 );
                 return servicesWrapper?.services.length ? (
                   <LocationService
