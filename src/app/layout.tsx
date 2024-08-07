2ef098f44fd3bc29f96aa43d0fadc08927d0db5e
Jacob Beard <jake@minnow.io>
2024-08-07 01:26:50 -0600

    Fix format

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,11 +6,11 @@
 
 import "./globals.css";
 import { GoogleAnalytics } from "@next/third-parties/google";
-import type { Viewport } from 'next'
- 
+import type { Viewport } from "next";
+
 export const viewport: Viewport = {
-  themeColor: '#FFD54F',
-}
+  themeColor: "#FFD54F",
+};
 
 const GOOGLE_ANALYTICS_MEASUREMENT_ID = process.env
   .GOOGLE_ANALYTICS_MEASUREMENT_ID as string;
