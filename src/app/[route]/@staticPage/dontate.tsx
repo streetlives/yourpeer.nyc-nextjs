46e9725c081fd1a5b179dfc5e86d2e56470982f8
Jacob Beard <jake@minnow.io>
2024-09-01 10:16:42 -0600

    Add paragraph to donate page

--- src/app/[route]/@staticPage/dontate.tsx
+++ src/app/[route]/@staticPage/dontate.tsx
@@ -4,9 +4,20 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
+"use client";
+
+import { getTargetLanguage, LanguageTranslationContext, LanguageTranslationContextType } from "@/components/language-translation-context";
 import { TranslatableText } from "@/components/translatable-text";
+import { useContext } from "react";
 
 export function DonationPage() {
+  const { gTranslateCookie } = useContext(
+    LanguageTranslationContext,
+  ) as LanguageTranslationContextType;
+
+  const targetLanguage = gTranslateCookie
+    ? getTargetLanguage(gTranslateCookie)
+    : null;
   return (
     <section className="bg-white py-12 pt-28 lg:pt-32 lg:py-20 flex-1">
       <div className="px-5 max-w-xl mx-auto">
@@ -20,6 +31,12 @@ export function DonationPage() {
           <p className="text-dark text-center mb-6">
             <TranslatableText text="Each $30 covers an hour's stipend for unhoused people or people with lived expertise to participate in Streetlives research or product testing." />
           </p>
+          {targetLanguage === "ru" ? (
+            <p className="text-dark text-center mb-6">
+              Страница будет показана на английском языке. Используйте Yandex
+              или Google переводчик.
+            </p>
+          ) : undefined}
           <div className="">
             <a
               href="https://opencollective.com/streetlives"
