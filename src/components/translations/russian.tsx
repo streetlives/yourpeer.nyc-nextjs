9a7c910bf44d504edea4703e67ce5530065037e2
Jacob Beard <jake@minnow.io>
2024-09-01 22:01:42 -0600

    Use a shorter version of placeholder text

--- src/components/translations/russian.tsx
+++ src/components/translations/russian.tsx
@@ -134,7 +134,7 @@ const translations: Record<string, string> = {
   "View list": "Открыть лист",
   "#filters-popup-age-label":
     "Сколько полных лет человеку, нуждающемуся в помощи?",
-  "Enter Age": "Если им 25 лет и 5 месяцев, например, введите 25, только цифры",
+  "Enter Age": "Округлите число лет в меньшую сторону",
   "People rely on social services for many reasons. Our information specialists all have lived experiences navigating the support system and apply their knowledge collecting the information you find here.  We’re building YourPeer so it's easier for you to find the right service.":
     "Многие нуждаются в помощи из-за непредсказуемых жизненных обстоятельств. Мы публикуем о бесплатных услугах на YourPeer, чтобы немного облегчить Ваш поиск.",
   "View Street View": "Открыть панорамный вид входной группы",
