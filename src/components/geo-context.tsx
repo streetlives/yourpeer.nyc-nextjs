8946061474639aa969acba0c74af2e92a9a7e372
Jacob Beard <jake@minnow.io>
2024-09-13 13:46:20 -0600

    Integrate geo analytics

--- src/components/geo-context.tsx
+++ src/components/geo-context.tsx
@@ -22,6 +22,57 @@ export type GeoCoordinatesContextType = {
   ) => void;
 };
 
+function getFirstAddressComponent(
+  results: google.maps.GeocoderResult[] | null,
+  addressComponentType: string,
+): string | undefined {
+  return (results || [])
+    .map((r) =>
+      r.address_components
+        .filter((ac) => ac.types.includes(addressComponentType))
+        .map((ac) => ac.long_name),
+    )
+    .reduce((a, b) => a.concat(b), [])
+    .pop();
+}
+
+function logGeoEvent(coords: Position): void {
+  fetch(`/geocode/analytics/all?latitude=${coords.lat}&longitude=${coords.lng}`)
+    .then((response) => response.json())
+    .then((geoAnalytics) => {
+      // convert lat/lng to custom event
+      const geocoder = new google.maps.Geocoder();
+      const location = new google.maps.LatLng(coords.lat, coords.lng);
+      // lookup neighborhood and borough from lat/lng
+      // we need to get neighborhood and borough
+      geocoder.geocode({ location }, (results, status) => {
+        if (status == "OK") {
+          // get the neighborhood
+          const googleNeighborhood = getFirstAddressComponent(
+            results,
+            "neighborhood",
+          );
+          const googleBorough = getFirstAddressComponent(
+            results,
+            "sublocality",
+          );
+          const zipCode = getFirstAddressComponent(results, "postal_code");
+          window["gtag"]("event", "geolocation", {
+            googleNeighborhood,
+            googleBorough,
+            neighborhood: geoAnalytics.neighborhood,
+            borough: geoAnalytics.borough,
+            zipCode,
+            schoolDistrict: geoAnalytics.districts.school,
+            congressionalDistrict: geoAnalytics.districts.congressional,
+            communityDistrict: geoAnalytics.districts.community,
+            pathname: window.location.pathname,
+          });
+        }
+      });
+    });
+}
+
 export const GeoCoordinatesProvider: React.FC<{
   children: React.ReactNode;
 }> = ({ children }) => {
@@ -35,11 +86,12 @@ export const GeoCoordinatesProvider: React.FC<{
     cookies.remove("longitude");
     window.navigator.geolocation.getCurrentPosition(
       (userPosition) => {
-        // TODO: logGeoEvent(pos.coords);
-        setUserPosition({
+        const position: Position = {
           lat: userPosition.coords.latitude,
           lng: userPosition.coords.longitude,
-        });
+        };
+        logGeoEvent(position);
+        setUserPosition(position);
         cookies.set("latitude", userPosition.coords.latitude.toString());
         cookies.set("longitude", userPosition.coords.longitude.toString());
         // TODO: we want to change the selection to "nearby"
